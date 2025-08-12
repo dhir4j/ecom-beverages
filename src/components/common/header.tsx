7abad4eb90932dc513ffdf2038e7d8ba9d08a562
Dhiraj Kapse <kapsedhiraj15@gmail.com>
2025-08-12 21:27:06 +0000

    move the cart dark mode toggle and search icon more to right  in mobile

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -209,7 +209,7 @@ function HeaderContent() {
             <span className="font-logo text-2xl font-bold whitespace-nowrap">SK Traders</span>
         </Link>
         
-        <div className="flex flex-1 justify-end items-center space-x-0">
+        <div className="flex flex-1 justify-end items-center">
             <Button variant="ghost" size="icon" onClick={handleSearchIconClick}>
                 <Search className="h-5 w-5" />
                 <span className="sr-only">Search</span>
