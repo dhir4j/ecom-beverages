095a5a2b51fffc94142549168aa1856da7e6f875
Dhiraj Kapse <kapsedhiraj15@gmail.com>
2025-08-12 20:40:10 +0000

    remove the hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,10 @@
 import { TrustBadges } from "@/components/home/trust-badges";
 import { TopPicks } from "@/components/home/top-picks";
 import { CategoryShowcase } from "@/components/home/category-showcase";
-import { Hero } from "@/components/home/hero";
 
 export default function Home() {
   return (
     <>
-      <Hero />
       <TopPicks />
       <CategoryShowcase />
       <TrustBadges />
