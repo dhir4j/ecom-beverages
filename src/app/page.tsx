3d3e0a57567fb241ff17e76f97a26e5ee3afe5c9
Dhiraj Kapse <kapsedhiraj15@gmail.com>
2025-08-12 19:38:51 +0000

    move the shop by category below the products lists

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@
 import { TrustBadges } from "@/components/home/trust-badges";
 import { TopPicks } from "@/components/home/top-picks";
 import { CategoryShowcase } from "@/components/home/category-showcase";
+import { Hero } from "@/components/home/hero";
 
 export default function Home() {
   return (
     <>
-      <CategoryShowcase />
+      <Hero />
       <TopPicks />
+      <CategoryShowcase />
       <TrustBadges />
     </>
   );
