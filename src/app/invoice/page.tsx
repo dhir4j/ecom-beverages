1de1a39e90354d430edfd8cb9db40bbf42a47c51
Dhiraj Kapse <kapsedhiraj15@gmail.com>
2025-08-12 19:48:04 +0000

    Creating an optimized production build ...  ✓ Compiled successfully in 1

--- src/app/invoice/page.tsx
+++ src/app/invoice/page.tsx
@@ -1,9 +1,13 @@
+
 import { InvoiceDetails } from "@/components/invoice/invoice-details";
+import { Suspense } from "react";
 
 export default function InvoicePage() {
   return (
     <div className="bg-muted/40">
-        <InvoiceDetails />
+        <Suspense fallback={<div>Loading invoice...</div>}>
+            <InvoiceDetails />
+        </Suspense>
     </div>
   );
 }
